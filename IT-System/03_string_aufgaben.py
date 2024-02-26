a54622d12d007bdd4ba97808bcbcd1fc88d59170
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2024-02-26 09:56:34 +0100

    Update 03_string_aufgaben.py

--- IT-System/03_string_aufgaben.py
+++ IT-System/03_string_aufgaben.py
@@ -20,13 +20,13 @@ print (hallo, welt)
 a = "abcdefghijklmnopqrstuvwxyz "
 
 # Erstelle eine Variable name und weise ihr den Vor- und Nachnamen bob studehi zu.
-name = "bob studehi"
+name =  a[1]+a[14]+a[1]+a[-1]+a[-1]+a[-9:-6]+a[3:5]+a[7:9]
 print (name)
 
 # Benutze dafür nur die Buchstaben aus der Variable alphabet
 # Ändere mithilfe der upper Methode den Anfangsbuchstaben des Vor- und Nachnamens
-teilen = name.split(" ")
-print (teilen)
+name = name[0].upper() + name[1:4] + name[4].upper() + name[5:]
+print(name)
 
 # Printe den Nachnahmen mit Slicen der name Variable
 leer_index = name.find(" ")
