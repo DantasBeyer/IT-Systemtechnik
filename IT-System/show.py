9c172c194a1e14edd99358da7831492cf6f8b311
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2024-02-26 08:08:29 +0100

    Add files via upload

---
+++ IT-System/show.py
@@ -0,0 +1,23 @@
+# name1 = "name1"
+# name2 = "name2"
+# name3 = "name3"
+# name4 = "name4"
+
+# value1 = 1
+# value2 = 2
+# value3 = 3
+
+# bob_autos = 1
+# bob_autos = bob_autos + 1
+# bob_autos += 10 
+
+# print(bob_autos)
+# print(name2)
+# print(value2 * value3)
+
+a, b, c,= 5, 3, 7
+
+print(c)
+print(b)
+print(a)
+
