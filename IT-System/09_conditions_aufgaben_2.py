c06e9ef9dbe4daa19da79fecd9faf661ab798ad0
DantasBeyer <dantasbeyer@hotmail.com>
2024-02-27 17:00:35 +0100

    update

--- IT-System/09_conditions_aufgaben_2.py
+++ IT-System/09_conditions_aufgaben_2.py
@@ -36,7 +36,7 @@
 # Schreiben Sie ein Programm, in das der Benutzer zunächst über die Eingabe der Buchstaben R, U oder I
 # auswählen kann, welche Größe berechnet werden soll. Gibt er einen falschen Buchstaben ein, soll eine Meldung
 # über die Fehleingabe erfolgen.
-# Anschließend soll er die Werte der fehlenden Größen eingeben. Am Ende gibt das Programm den Wert der
+# Anschließend soll er die pricee der fehlenden Größen eingeben. Am Ende gibt das Programm den price der
 # gesuchten Größe mit der richtigen Einheit aus
 
 
@@ -44,19 +44,19 @@
 #spannung: U = "R" * "I", stärke: I = "U" / "R", wiederstand: R = "U" / "I"
 
 
-# u_r_i = input("gibt ein wert ein U, R oder I: ").upper()
+# u_r_i = input("gibt ein price ein U, R oder I: ").upper()
     
 # if u_r_i == "U":
-# 	r = float(input("gibt ein wert für R ein: "))
-# 	i = float(input("gibt ein wert für I ein: "))
+# 	r = float(input("gibt ein price für R ein: "))
+# 	i = float(input("gibt ein price für I ein: "))
 # 	print("Die Spannung ist ", r*i, "Volt" )
 # elif u_r_i == "I":
-# 	u = float(input("gibt ein wert für U ein: "))
-# 	r = float(input("gibt ein wert für R ein: "))
+# 	u = float(input("gibt ein price für U ein: "))
+# 	r = float(input("gibt ein price für R ein: "))
 # 	print("Die Stärke ist ", u/r, "Ohm")
 # elif u_r_i == "R":
-#     u = float(input("gibt ein wert für U ein: "))
-#     i = float(input("gibt ein wert für I ein: "))
+#     u = float(input("gibt ein price für U ein: "))
+#     i = float(input("gibt ein price für I ein: "))
 #     print("Der Wiederstang ist ", u/i, "Ampere")
 # else:
 # 	print("falsche eingabe, bitte U, R oder I eingeben ")
@@ -64,19 +64,31 @@
 
 
 
-# Ein Hardware-Großhändler führt ein Rabattsystem für Stammkunden ein: Liegt der Bestellwert zwischen 0 und
-# 100 €, erhält der Kunde einen Rabatt von 10 %. Liegt der Bestellwert höher, aber insgesamt nicht über 500 €,
-# beträgt der Rabatt 15 %, in allen anderen Fällen beträgt der Rabatt 20 %. Nach Eingabe des Bestellwertes soll
-# der ermäßigte Bestellwert berechnet und ausgegeben werden.
+# Ein Hardware-Großhändler führt ein Rabattsystem für Stammkunden ein: Liegt der Bestellprice zwischen 0 und
+# 100 €, erhält der Kunde einen Rabatt von 10 %. Liegt der Bestellprice höher, aber insgesamt nicht über 500 €,
+# beträgt der Rabatt 15 %, in allen anderen Fällen beträgt der Rabatt 20 %. Nach Eingabe des Bestellpricees soll
+# der ermäßigte Bestellprice berechnet und ausgegeben werden.
 
 
-promotion_1  = 10
-promotion_2 = 15
-promotion_3 = 20
-  
+value_1 = 100
+value_2 = 500
+discount_1 = 0.10
+discount_2 = 0.15
+discount_3 = 0.20
 
+price = float(input( "Gibt ein Bestellprice  ein :" ))
 
+if price <= value_1:
+    promotion = discount_1
+elif price <= value_2:
+    promotion = discount_2
+else:
+    promotion = discount_3
+endprice = price * (1 - promotion)
 
+print(f"Preis ohne rabatt : {price:.2f} €")
+print(f"Rabatt : {promotion*100} %")
+print(f"Endpreis mit rabatt : {endprice:.2f} €")
 
 # Der BMI berechnet sich aus dem Körpergewicht [kg] dividiert durch das Quadrat der Körpergröße [m2
 # Die Formel lautet: BMI = (Körpergewicht in kg): (Körpergröße in m)**2
@@ -100,13 +112,13 @@ promotion_3 = 20
 #
 #
 # Bedingung 1:
-# Ist eine Jahreszahl ganzzahlig durch 4 teilbar, dann ist das Jahr ein Schaltjahr. Testwerte: 1720, 1972 und 1980 waren Schaltjahre.
+# Ist eine Jahreszahl ganzzahlig durch 4 teilbar, dann ist das Jahr ein Schaltjahr. Testpricee: 1720, 1972 und 1980 waren Schaltjahre.
 # Bedingung 2:
 # Ausnahme zu den Jahreszahlen, die der Bedingung 1 genügen, sind alle Jahreszahlen, die nach Bedingung 1 ein Schaltjahr sind, aber deren Jahreszahl ganzzahlig durch 100 teilbar ist.
-# Testwerte: 1700, 1800 und 1900 oder ferner 2100 sind keine Schaltjahre.
+# Testpricee: 1700, 1800 und 1900 oder ferner 2100 sind keine Schaltjahre.
 # Bedingung 3:
 # Ausnahme zu den Jahreszahlen, die der Bedingung 2 genügen, sind alle Jahreszahlen, die nach Bedingung 2 kein Schaltjahr sind, aber deren Jahreszahl ganzzahlig durch 400 teilbar.
-# Testwerte: 1600 und 2000 waren Schaltjahre.
+# Testpricee: 1600 und 2000 waren Schaltjahre.
 #1980 2100 1200
 #jahr = int(input('Bitte gebe ein Jahr an\n'))
 
