94f76a1f09c901361f6fdb5c3e99dbb5ef1dff70
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2024-02-27 09:26:21 +0100

    Add files via upload

--- IT-System/donald.py
+++ IT-System/donald.py
@@ -5,8 +5,10 @@ name_teilen = name.split(" ")
 vorname = name_teilen[0]
 nachname = name_teilen[1]
 
+# Vertausche die ersten Buchstaben: 'D' von "Donald" mit 'T' von "Trump"
 neuer_vorname = nachname[0]+ vorname[1:]
 neuer_nachname = vorname[0] + nachname[1:]
 
+# Setze die Teile wieder zusammen
 neuer_name = neuer_vorname + " " + neuer_nachname
 print(neuer_name)
