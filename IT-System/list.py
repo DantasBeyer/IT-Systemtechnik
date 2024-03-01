a737820637a96a33f99df42c3c712816947ed797
DantasBeyer <dantasbeyer@hotmail.com>
2024-03-01 06:59:18 +0100

    updated

---
+++ IT-System/list.py
@@ -0,0 +1,14 @@
+my_list = [50, 80.80 , True, "string", True]
+my_tuple = (50, 60, 50, 80)
+my_set = {50, 50, 60, 40, 90, 80}
+my_dic = {"key1":50, "key2":60, "key3": True, "key4":50, "key":70}
+
+my_list.pop()
+
+my_list[0] = 80
+# my_tuple[0] = 80
+my_dic["key1"] = "Joao"
+print(my_list)
+print(my_tuple)
+print(my_set)
+print(my_dic)
